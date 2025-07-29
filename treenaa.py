4c8355f292162daeda41d8ec03932d295ccffbb9
Elkkuboi <elias@ervamaa.net>
2025-07-29 23:07:41 +0300

    kirjoitettiin sanalliset ohjeet miten koodaan tiedoston

--- treenaa.py
+++ treenaa.py
@@ -1,6 +1,19 @@
 '''Täällä bottia opetetaan pelaamaan, botti tallentaa oppimansa sanakirjana JSON tiedostoon'''
 '''
-Pistetään botti pelaamaan itseään vastaan
+Pistetään botti pelaamaan itseään vastaan, haetaan robotti luokka ja luodaan olio X:lle ja O:lle
+tehdään suuri while looppi, jossa pelataan anettu määrä pelejä
+haetaan tiedot (arvot.json)
+
+jokainen peli kulkee näin:
+tyhjä pelikenttä
+valitaan siirto
+pelataan siirto
+annetaan palautetta
+tarkistetaan päättyikö peli, jos päättyi anna palautetta
+vaihda vuoroa
+
+kun pelit on pelattu, niin tallennetaan tulokset
+ilmoitus
 '''
 
 
