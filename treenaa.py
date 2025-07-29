5b5be70df9dafddece342a47b2afe27b1462f0d9
Elkkuboi <elias@ervamaa.net>
2025-07-29 22:56:12 +0300

    luotiin treenaustiedosto

---
+++ treenaa.py
@@ -0,0 +1,9 @@
+'''Täällä bottia opetetaan pelaamaan, botti tallentaa oppimansa sanakirjana JSON tiedostoon'''
+'''
+Pistetään botti pelaamaan itseään vastaan
+'''
+
+
+
+from peli import robotti, luo_lauta, hae_mahdolliset_siirrot, tee_siirto, tarkista_voitto, tarkista_tasapeli
+
